15fc5bc32e331e2cc5412673363358f41debe2ad
Zaiko Eugene <euhene@linnovate.net>
2023-07-09 20:52:11 +0300

    feat: change link on one person

--- src/components/scripts/data.js
+++ src/components/scripts/data.js
@@ -128,7 +128,7 @@ let poets = [
     id: 17,
   },
   {
-    src: "http://www.kormanews.by/wp-content/uploads/2014/09/%D0%94%D0%B5%D1%80%D1%83%D0%B6%D0%B8%D0%BD%D1%81%D0%BA%D0%B8%D0%B9.jpg",
+    src: "https://zviazda.by/sites/default/files/field/image/5_26_dzeruzhynski.jpg",
     name: "Авяр’ян Сафонавіч",
     description:
       "Беларускі пісьменнік і публіцыст, аўтар раманаў і апавяданняў, у якіх ён даследуе палітычныя і сацыяльныя праблемы сучаснага суполкавага жыцця.",
